5cded99174ed2acb33b391214bd9974be7e47c0f
nequin <n3qu1n@gmail.com>
2022-06-08 21:17:03 -0700

    Fix contract and tests

--- contracts/CitadelRaffle.sol
+++ contracts/CitadelRaffle.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/access/ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CitadelRaffle is Ownable {
     uint256 public MIN = 0;
@@ -10,7 +10,7 @@ contract CitadelRaffle is Ownable {
     }
 
     function random() private view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, now, MIN, MAX)));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, MIN, MAX)));
     }
 
     function updateParameters(uint256 _min, uint256 _max) public onlyOwner {
@@ -18,9 +18,9 @@ contract CitadelRaffle is Ownable {
         MAX = _max;
     }
 
-    function raffle() public onlyOwner {
-        uint256 random = random();
-        uint256 winner = random % (MAX - MIN + 1) + MIN;
+    function raffle() public view onlyOwner returns(uint256) {
+        uint256 _random = random();
+        uint256 winner = (_random % (MAX - MIN + 1)) + MIN;
 
         return winner;
     }
